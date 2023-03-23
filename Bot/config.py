fa598db256599a2fe8a83b71c3c46e0834d39890
Rufina <afina_sabakhova@mail.ru>
2023-03-23 05:37:29 +0300

    config changed

--- Bot/config.py
+++ Bot/config.py
@@ -1,7 +1,7 @@
 from pytesseract import pytesseract
 import pyautogui
 
-pytesseract.tesseract_cmd = r'C:\Users\Dima vse horosho\AppData\Local\Tesseract-OCR\tesseract.exe'
+pytesseract.tesseract_cmd = r'C:\Users\Dimaslav\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
 # разрешение экрана, умноженное на 2 - для лучшего чтения данных с изображения
 WIDTH = pyautogui.size().width * 2
@@ -12,8 +12,13 @@ PATH_SCREENSHOT = r"screenshots\screen.png"
 PATH_SCHEDULE_SCREENSHOT = r"screenshots\scheduler_screen.png"
 
 # время в минутах, через которое программа будет делать скриншот по расписанию
-INTERVAL_TIME = 2
+INTERVAL_TIME = 1
 
 # кол-во рабочих столов
 DESKS = 1
+
+# регулярное выражение
+REG_EXP = r"(вы погиб?)|(погиб)"
+
+
 
