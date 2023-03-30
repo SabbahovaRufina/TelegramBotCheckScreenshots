70f088c264fb3bab37ca501db1d2ea930db786d7
Rufina <afina_sabakhova@mail.ru>
2023-03-30 10:21:56 +0300

    loop_desks changed

--- Bot/main_functions.py
+++ Bot/main_functions.py
@@ -3,6 +3,7 @@ from JSONFile import JSONFile
 from config import PATH_SCHEDULE_SCREENSHOT, PATH_SCREENSHOT, DESKS
 from handlers import send_message
 from pyautogui import hotkey
+from asyncio import sleep
 
 
 async def get_message_text(user_id, n):
@@ -24,21 +25,23 @@ async def get_new_screenshot(user_id, path=PATH_SCREENSHOT):
         return None
 
 
-async def make_screenshot(desk, path=PATH_SCHEDULE_SCREENSHOT):
+async def make_screenshot(path=PATH_SCHEDULE_SCREENSHOT):
     screen = Screenshot(path)
     file = JSONFile()
     await screen.get_screenshot()
     if await screen.is_right():
         for chat in await file.get_work_users():
-            await send_message(chat, f"Возможно, вы погибли. Рабочий стол {desk}", screen.path)
+            await send_message(chat, f"Возможно, вы погибли", screen.path)
 
 
 async def loop_desks(desks=DESKS):
     assert desks >= 1
-    for desk in range(desks):
-        if desk != 0:
-            hotkey('ctrl', 'win', 'right')
-        await make_screenshot(desk + 1)
+    for desk in range(desks - 1):
+        hotkey('ctrl', 'win', 'right')
+        await sleep(3)
+        await make_screenshot()
     else:
         for _ in range(desks - 1):
+            await sleep(3)
             hotkey('ctrl', 'win', 'left')
+
